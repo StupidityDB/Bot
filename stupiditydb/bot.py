fdca7d4c470a6599d2e63807008c6fe5635ec0a5
mugman <53713683+mugman174@users.noreply.github.com>
2023-03-14 08:14:30 -0700

    formatting

--- stupiditydb/bot.py
+++ stupiditydb/bot.py
@@ -4,23 +4,34 @@ from discord.ext import commands
 import discord
 import config
 
+
 class Bot(commands.Bot):
     def __init__(self, intents: discord.Intents, **kwargs):
-        super().__init__(command_prefix=commands.when_mentioned_or('>'), intents=intents, **kwargs)
+        super().__init__(
+            command_prefix=commands.when_mentioned_or(">"), intents=intents, **kwargs
+        )
 
     async def setup_hook(self):
         for cog in config.cogs:
             try:
                 await self.load_extension(cog)
             except Exception as exc:
-                print(f'Could not load extension {cog} due to {exc.__class__.__name__}: {exc}')
+                print(
+                    f"Could not load extension {cog} due to {exc.__class__.__name__}: {exc}"
+                )
 
     async def on_ready(self):
-        print(f'Logged on as {self.user} (ID: {self.user.id})')
+        print(f"Logged on as {self.user} (ID: {self.user.id})")
 
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.NotOwner):
-            await ctx.reply(embed=discord.Embed(title="Error",description=f"🤯 You are not allowed to run this command."))
+            await ctx.reply(
+                embed=discord.Embed(
+                    title="Error",
+                    description=f"🤯 You are not allowed to run this command.",
+                )
+            )
+
 
 intents = discord.Intents.default()
 intents.message_content = True
--- stupiditydb/cogs/utility.py
+++ stupiditydb/cogs/utility.py
@@ -6,6 +6,7 @@ from contextlib import redirect_stdout
 from discord.ext import commands
 import discord
 
+
 class Utility(commands.Cog):
     """Simple Utilities"""
 
@@ -16,8 +17,8 @@ class Utility(commands.Cog):
     def cleanup_code(self, content: str) -> str:
         """Automatically removes code blocks from the code."""
         # remove ```py\n```
-        if content.startswith('```') and content.endswith('```'):
-            return '\n'.join(content.split('\n')[1:-1])
+        if content.startswith("```") and content.endswith("```"):
+            return "\n".join(content.split("\n")[1:-1])
 
     @commands.hybrid_command()
     async def ping(self, ctx):
@@ -27,27 +28,25 @@ class Utility(commands.Cog):
     @commands.command(hidden=True)
     @commands.is_owner()
     async def reload(self, ctx, module):
-        module = f"cogs.{module}"
         try:
             await self.bot.reload_extension(module)
         except commands.ExtensionNotLoaded:
             await self.bot.load_extension(module)
         await ctx.reply("Reloaded")
 
-
-    @commands.command(hidden=True, name='eval')
+    @commands.command(hidden=True, name="eval")
     @commands.is_owner()
     async def _eval(self, ctx, *, body: str):
         """Evaluates a code"""
 
         env = {
-            'bot': self.bot,
-            'ctx': ctx,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            'guild': ctx.guild,
-            'message': ctx.message,
-            '_': self._last_result,
+            "bot": self.bot,
+            "ctx": ctx,
+            "channel": ctx.channel,
+            "author": ctx.author,
+            "guild": ctx.guild,
+            "message": ctx.message,
+            "_": self._last_result,
         }
 
         env.update(globals())
@@ -60,28 +59,29 @@ class Utility(commands.Cog):
         try:
             exec(to_compile, env)
         except Exception as e:
-            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            return await ctx.send(f"```py\n{e.__class__.__name__}: {e}\n```")
 
-        func = env['func']
+        func = env["func"]
         try:
             with redirect_stdout(stdout):
                 ret = await func()
         except Exception as e:
             value = stdout.getvalue()
-            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+            await ctx.send(f"```py\n{value}{traceback.format_exc()}\n```")
         else:
             value = stdout.getvalue()
             try:
-                await ctx.message.add_reaction('\u2705')
+                await ctx.message.add_reaction("\u2705")
             except:
                 pass
 
             if ret is None:
                 if value:
-                    await ctx.send(f'```py\n{value}\n```')
+                    await ctx.send(f"```py\n{value}\n```")
             else:
                 self._last_result = ret
-                await ctx.send(f'```py\n{value}{ret}\n```')
+                await ctx.send(f"```py\n{value}{ret}\n```")
+
 
 async def setup(bot):
     await bot.add_cog(Utility(bot))
