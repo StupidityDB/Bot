0e4841ad9544a194cf9b680fd767c8e9bd1909f6
mugman <53713683+mugman174@users.noreply.github.com>
2023-03-15 11:34:02 -0700

    [windows] can specify multiple windows versions

--- stupiditydb/cogs/windows10.py
+++ stupiditydb/cogs/windows10.py
@@ -1,5 +1,9 @@
+from typing import Literal
+
 from discord.ext import commands
 import discord
+from discord import app_commands
+
 
 class Windows10(commands.Cog):
     """Windows 10 Downloader"""
@@ -8,9 +12,15 @@ class Windows10(commands.Cog):
         self.bot = bot
 
     @commands.hybrid_command()
-    async def windows10(self, ctx):
-        """Get Windows 10 for FREE! Directly from the Microsoft website. No Virus, Real, Microsoft Official, Windows 10, Official Download 2023"""
-        await ctx.send("https://microsoft.com/software-download/windows10ISO\nNote:\n> If you are using Microsoft Windows, spoof your user agent to a different operating system for the best results.")
+    @app_commands.describe(version="Windows Version")
+    async def windows(self, ctx, version: Literal["11", "10", "8.1"]):
+        """Get Windows 10 for FREE! Directly from the Microsoft website."""
+        version_dict = {"10": 10, "8.1": 8, "11": 11}
+        version = version_dict[version]
+        await ctx.send(
+            f"https://microsoft.com/software-download/windows{version}\nNote:\n> If you are using Microsoft Windows, spoof your user agent to a different operating system for the best results."
+        )
+
 
 async def setup(bot):
     await bot.add_cog(Windows10(bot))
